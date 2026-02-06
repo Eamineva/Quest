c8089a339c8dff49bc4867f21e869249e941391d
Elvira <elvira-amineva@mail.ru>
2026-02-06 11:07:55 +0500

    init

--- src/main/java/com/quest/model/GameService.java
+++ src/main/java/com/quest/model/GameService.java
@@ -14,28 +14,18 @@ public class GameService {
         // Начальный шаг
         Step start = new Step("start", "Начало приключения",
                 "Вы мечтаете о поездке на море. Солнце, пляж, шум волн... " +
-                        "Но сначала нужно подготовиться к путешествию! С чего начнем?",
+                        "Но сначала нужно подготовиться к путешествию!",
                 false, false);
-        start.addChoice("buy_ticket", "Купить билет на самолет", "ticket_step");
+        start.addChoice("buy_ticket", "Купить билет на самолет", "suitcase_step");  // сразу на suitcase_step
         start.addChoice("skip_ticket", "Решить купить билет позже", "lost_no_ticket");
         steps.put("start", start);
 
-        // Шаг с билетом
-        Step ticketStep = new Step("ticket_step", "Покупка билета",
-                "Вы зашли на сайт авиакомпании. Билеты на море стоят дорого, но мечта того стоит! " +
-                        "Что выберете?",
-                false, false);
-        ticketStep.addChoice("buy", "Купить билет (5000 руб.)", "suitcase_step");
-        ticketStep.addChoice("dont_buy", "Передумать, слишком дорого", "lost_no_ticket");
-        steps.put("ticket_step", ticketStep);
-
-        // Шаг с чемоданом
+        // Шаг с чемоданом (сразу после покупки билета)
         Step suitcaseStep = new Step("suitcase_step", "Сбор чемодана",
-                "Ура! Билет куплен. Теперь нужно собрать чемодан. " +
-                        "Что будете упаковывать?",
+                "Вы купили билет! Теперь нужно собрать чемодан. ",
                 false, false);
-        suitcaseStep.addChoice("pack", "Тщательно собрать все необходимое", "passport_step");
-        suitcaseStep.addChoice("forget", "Решить собрать в последний момент", "lost_no_suitcase");
+        suitcaseStep.addChoice("pack", "Собрать все необходимое", "passport_step");
+        suitcaseStep.addChoice("forget", "Собрать в последний момент", "lost_no_suitcase");
         steps.put("suitcase_step", suitcaseStep);
 
         // Шаг с паспортом
@@ -43,28 +33,20 @@ public class GameService {
                 "Чемодан собран. Осталось проверить самый важный документ для полета за границу.",
                 false, false);
         passportStep.addChoice("check", "Проверить паспорт и положить в сумку", "airport_step");
-        passportStep.addChoice("ignore", "Не проверять, наверное он где-то есть", "lost_no_passport");
+        passportStep.addChoice("ignore", "Не проверять", "lost_no_passport");
         steps.put("passport_step", passportStep);
 
         // Шаг аэропорта
         Step airportStep = new Step("airport_step", "Аэропорт",
                 "Вы приехали в аэропорт. Впереди контроль безопасности и паспортный контроль.",
                 false, false);
-        airportStep.addChoice("security", "Пройти контроль безопасности", "flight_step");
+        airportStep.addChoice("security", "Пройти контроль безопасности", "boarding_step");  // сразу на boarding_step
         airportStep.addChoice("skip_security", "Попробовать пройти без контроля", "lost_security");
         steps.put("airport_step", airportStep);
 
-        // Шаг рейса
-        Step flightStep = new Step("flight_step", "Регистрация на рейс",
-                "Вы прошли контроль! Осталось зарегистрироваться на рейс и пройти к гейту.",
-                false, false);
-        flightStep.addChoice("on_time", "Вовремя пройти регистрацию и отправиться к гейту", "boarding_step");
-        flightStep.addChoice("late", "Задержаться в дьюти-фри", "lost_late");
-        steps.put("flight_step", flightStep);
-
-        // Шаг посадки
+        // Шаг посадки (без отдельного шага рейса)
         Step boardingStep = new Step("boarding_step", "Посадка в самолет",
-                "Вы у гейта! Начинается посадка на рейс до теплого моря.",
+                "Вы прошли контроль безопасности! Начинается посадка на рейс.",
                 false, false);
         boardingStep.addChoice("board", "Сесть в самолет", "sea_step");
         boardingStep.addChoice("miss", "Опоздать на посадку", "lost_late");
@@ -89,7 +71,7 @@ public class GameService {
         // Нет чемодана
         Step lostNoSuitcase = new Step("lost_no_suitcase", "Что брать с собой?",
                 "Вы приехали в аэропорт без чемодана. Нечего надеть на пляже! " +
-                        "Путешествие отменяется. В следующий раз соберитесь лучше!",
+                        "Путешествие отменяется.",
                 true, false);
         steps.put("lost_no_suitcase", lostNoSuitcase);
 
@@ -109,8 +91,7 @@ public class GameService {
 
         // Опоздание
         Step lostLate = new Step("lost_late", "Опоздали на рейс",
-                "Вы не успели на посадку! Самолет улетел без вас. " +
-                        "Следующий рейс только через неделю... Каникулы испорчены.",
+                "Вы не успели на посадку! Самолет улетел без вас. " ,
                 true, false);
         steps.put("lost_late", lostLate);
     }
@@ -140,8 +121,9 @@ public class GameService {
 
     private void updateGameState(GameState state, String choiceId, String nextStepId) {
         switch (state.getCurrentStepId()) {
-            case "ticket_step":
-                if ("buy".equals(choiceId)) {
+            case "start":
+                // Если купили билет в первом шаге
+                if ("buy_ticket".equals(choiceId)) {
                     state.setHasTicket(true);
                 }
                 break;
@@ -160,9 +142,8 @@ public class GameService {
                     state.setPassedSecurity(true);
                 }
                 break;
-            case "flight_step":
             case "boarding_step":
-                if ("on_time".equals(choiceId) || "board".equals(choiceId)) {
+                if ("board".equals(choiceId)) {
                     state.setOnTime(true);
                 }
                 break;
